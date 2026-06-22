471c5f26e41d3e12b91ab89895fd2dd787a4121c
Farhan Ali <mfarhanraj7860@gmail.com>
2026-06-23 00:21:01 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+
+st.title("MNIST Digit Classifier")
+
+@st.cache_resource
+def load_model():
+    return tf.keras.models.load_model("mnist_model.keras")
+
+st.write("Upload a 28x28 grayscale digit image.")
+
+uploaded = st.file_uploader("Choose image", type=["png","jpg","jpeg"])
+
+if uploaded:
+    img = Image.open(uploaded).convert("L").resize((28,28))
+    st.image(img, caption="Input Image", width=150)
+
+    arr = np.array(img).astype("float32") / 255.0
+    arr = arr.reshape(1, 784)
+
+    model = load_model()
+    pred = model.predict(arr, verbose=0)
+    digit = int(np.argmax(pred))
+
+    st.success(f"Predicted Digit: {digit}")
